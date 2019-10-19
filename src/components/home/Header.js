94cd452e25bac6f3f74ba04c055ab42f63db8acb
rinagreen <cathrinaumali@gmail.com>
2019-10-19 23:00:37 +0800

    first commit

---
+++ src/components/home/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import NavBar from "./NavBar.js";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <section id="home" className="col-md-12">
+        <div className="slider">
+          <div className="slide1"></div>
+          <div className="slide2"></div>
+          <div className="slide3"></div>
+          <div className="slide4"></div>
+          <div className="slide5"></div>
+        </div>
+
+        <div className="overhome col-md-12">
+          <div id="cName" className="col-md-7 ">
+            <div className="call">
+              <h1 style={call_style}>L'assiette Dujour</h1>
+            </div>
+            <p>The Finest French Cuisine and more </p>
+            <p> Best in the city. Friendly team. Stylish setting</p>
+            <div className="action">
+              <a href="#menu" style={action_style}>
+                {" "}
+                Discover our menu{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+}
+
+const action_style = {
+  fontSize: 20,
+  textDecoration: "none",
+  color: "#d9b04f"
+};
+
+const call_style = {
+  fontSize: 100,
+  fontFamily: "'Lobster', cursive"
+};
+
+export default Header;
---
+++ src/components/home/NavBar.js
@@ -0,0 +1,82 @@
+import React from "react";
+import fb_svg from "../../images/fb.svg";
+import tw_svg from "../../images/twitter.svg";
+import insta_svg from "../../images/insta.svg";
+
+function NavBar() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a className="homenav" href="#home">
+            <div className="circle"></div>
+            <span className="htooltip"> HOME </span>
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a className="homenav" href="#about">
+            <div className="circle"> </div>
+            <span className="htooltip"> ABOUT US </span>
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a className="homenav" href="#menu">
+            <div className="circle"> </div>
+            <span className="htooltip"> MENU </span>
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a className="homenav" href="#gallery">
+            <div className="circle"> </div>
+            <span className="htooltip"> GALLERY </span>
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a className="homenav" href="#place">
+            <div className="circle"> </div>
+            <span className="htooltip"> OUR PLACE </span>
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a className="homenav" href="#location">
+            <div className="circle"> </div>
+            <span className="htooltip"> LOCATION </span>{" "}
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a className="homenav" href="#contactus">
+            <div className="circle"> </div>
+            <span className="htooltip"> CONTACT US </span>
+          </a>
+        </li>{" "}
+        <br />
+        <li>
+          <a className="homenav" href="https://www.facebook.com/">
+            <img src={fb_svg} />
+            <span className="htooltip"> FACEBOOK </span>
+          </a>
+        </li>
+        <li>
+          <a className="homenav" href="https://www.twitter.com/">
+            <img src={tw_svg} />
+            <span className="htooltip"> TWITTER </span>
+          </a>
+        </li>
+        <li>
+          <a className="homenav" href="https://www.instagram.com/instagram/">
+            <img src={insta_svg} />
+            <span className="htooltip"> INSTAGRAM </span>{" "}
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default NavBar;
